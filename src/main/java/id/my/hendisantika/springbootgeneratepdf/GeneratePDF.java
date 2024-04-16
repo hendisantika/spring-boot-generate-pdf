b18f5c6571e466f2460493e2de05a61d312be24d
hendisantika <hendisantika@yahoo.co.id>
2024-04-16 09:35:28 +0700

    Change Groovy to Java 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootgeneratepdf/GeneratePDF.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.springbootgeneratepdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-generate-pdf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/16/24
+ * Time: 09:34
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootApplication
+public class GeneratePDF {
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratePDF.class, args);
+    }
+}
