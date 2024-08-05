fd5579b50503ba9a719c4d4dffbffd9a48ed0dd3
hendisantika <hendisantika@yahoo.co.id>
2024-08-06 06:58:05 +0700

    Add export in PdfService 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/springbootgeneratepdf/service/PdfService.java
+++ src/main/java/id/my/hendisantika/springbootgeneratepdf/service/PdfService.java
@@ -1,10 +1,18 @@
 package id.my.hendisantika.springbootgeneratepdf.service;
 
+import com.lowagie.text.Document;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfWriter;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Service;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,5 +35,31 @@ public class PdfService {
         renderer.finishPDF();
         ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
         return inputStream;
+    }
+
+    public void export(HttpServletResponse httpServletResponse) throws IOException {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, httpServletResponse.getOutputStream());
+
+        document.open();
+        Font fontTitle = FontFactory.getFont(FontFactory.COURIER_BOLD);
+        fontTitle.setSize(16);
+        fontTitle.setColor(108, 142, 163);
+
+        Paragraph paragraph = new Paragraph("Generate a PDF Using Spring Boot", fontTitle);
+        paragraph.setAlignment(Paragraph.ALIGN_CENTER);
+
+        Font fontParagraph = FontFactory.getFont(FontFactory.HELVETICA);
+        fontParagraph.setSize(12);
+        fontParagraph.setColor(153, 153, 255);
+
+        Paragraph headingPara = new Paragraph("This is Heading for a Paragraph.", fontParagraph);
+        headingPara.setAlignment(Paragraph.ALIGN_LEFT);
+
+        document.add(paragraph);
+        document.add(headingPara);
+
+        document.close();
+
     }
 }
